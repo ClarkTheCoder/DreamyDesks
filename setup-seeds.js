c2346c52aefbb31cc60596fa0fb932f28e807d86
ClarkTheCoder <carsonclark2009@gmail.com>
2021-08-25 12:51:51 -0600

    added and fixed CRUD operations

--- setup-seeds.js
+++ setup-seeds.js
@@ -8,32 +8,32 @@ mongoose.connect('mongodb://localhost:27017/dreamy-desks', {useNewUrlParser: tru
 
 const seedInfo = [
     {
-        name: 'Beastly PC gaming setup',
+        title: 'Beastly PC gaming setup',
         description: 'My beast ass gaming pc setup',
         category: 'gaming'
     },
     {
-        name: 'Mac Setup for Work',
+        title: 'Mac Setup for Work',
         description: 'This is my clean minimal Mac setup for my work as a designer',
         category: 'apple'
     },
     {
-        name: 'This is my chill station',
+        title: 'This is my chill station',
         description: 'This is where I watch porn and anime',
         category: 'leisure'
     },
     {
-        name: 'Lenovo Land',
+        title: 'Lenovo Land',
         description: 'This is my user upgraded Lenovo which I use for programming!',
         category: 'programming'
     },
     {
-        name: 'iPad Paradise',
+        title: 'iPad Paradise',
         description: 'This is where I relax with my iPad every day',
         category: 'apple'
     },
     {
-        name: 'Mac Mini setup',
+        title: 'Mac Mini setup',
         description: 'My setup which features a new Mac Mini m1x!',
         category: 'apple'
     },
